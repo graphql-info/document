03e085f4d6eefc677abb5241f07bb38cfecfdef1
Ilya Volodin <ilya.volodin@venmo.com>
2021-03-13 21:50:32 -0500

    Changes to plugin signature

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-const { html, unsafeHTML } = require('lit-html');
+const { html } = require('@popeindustries/lit-html-server');
+const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsafe-html');
 const prism = require('prismjs');
 const loadLanguages = require('prismjs/components/');
 const generateQuery = require('./queryGenerator');
 
 loadLanguages(['graphql']);
 
-module.exports = (page, schema) => {
-    const query = generateQuery(page.name, page.type === 'query' ? 'Query' : 'Mutation', '', {}, {}, [], 0, schema);
-    page.page.push({
-        name: 'input',
-        type: 'lit-html',
-        value: html`
-            <div class="documents">
-                <h3>Documents:</h3>
-                <section class="code">
-                    <pre class="language-graphql">${unsafeHTML(prism.highlight(query.queryStr, prism.languages.graphql, 'graphql'))}</pre>
-                </section>
-            </div>`
-    });
-    return page.page;
+module.exports = {
+    render: (ref, schema, type, originalRenderer) => {
+        const page = originalRenderer(ref, schema);
+        const query = generateQuery(ref.name, type === 'query' ? 'Query' : 'Mutation', '', {}, {}, [], 0, schema);
+        page.push({
+            name: 'documents',
+            type: 'lit-html',
+            value: html`
+                <div class="documents">
+                    <h3>Documents:</h3>
+                    <section class="code">
+                        <pre class="language-graphql">${unsafeHTML(prism.highlight(query.queryStr, prism.languages.graphql, 'graphql'))}</pre>
+                    </section>
+                </div>`
+        });
+        return page;
+    }
 };
